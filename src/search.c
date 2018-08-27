0d2a2b1edaf3290f562ca1da54c0c86bdd605db3
laldon <eatpraylove@women-at-work.org>
2018-08-27 23:41:35 +0200

    .

--- src/search.c
+++ src/search.c
@@ -378,10 +378,9 @@ int search(Thread* thread, PVariation* pv, int alpha, int beta, int depth, int h
         &&  hasNonPawnMaterial(board, board->turn)
         &&  thread->moveStack[height-1] != NULL_MOVE
         &&  thread->moveStack[height-2] != NULL_MOVE
-		&&  thread->moveStack[height-3] != NULL_MOVE
         && (!ttHit || !(ttBound & BOUND_UPPER) || ttValue >= beta)) {
 
-        R = 4 + depth / 6 + MIN(3, (eval - beta) / 200);
+        R = 3 + depth / 6 + MIN(3, (eval - beta) / 200);
 
         applyNullMove(board, undo);
 
