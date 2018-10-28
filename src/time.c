96ba4f459f423dfc30fd8ea627d44814f6d7ad35
laldon <eatpraylove@women-at-work.org>
2018-10-28 12:16:29 +0100

    .

--- src/time.c
+++ src/time.c
@@ -62,7 +62,7 @@ void initTimeManagment(SearchInfo* info, Limits* limits){
     if (limits->limitedBySelf){
 
         // Playing using X / Y or X / Y + Z time controls
-        if (limits->mtg >= 0){
+        if (limits->mtg > 0){
             info->idealUsage =  0.75 * limits->time / (limits->mtg +  5) + limits->inc;
             info->maxAlloc   =  4.00 * limits->time / (limits->mtg +  7) + limits->inc;
             info->maxUsage   = 10.00 * limits->time / (limits->mtg + 10) + limits->inc;
